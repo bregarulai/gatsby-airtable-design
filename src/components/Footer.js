fc792aa742471eca0ae2b096c0d8e012aded4b26
bregarulai <bregarulai@gmail.com>
2020-11-10 10:53:09 -0500

    initial setup for hero component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ const Footer = () => {
   return (
     <Wrapper>
       <p>
-        &copy; {new Date().getFullYear()} John Smilga. All rights reserved.
-        Built with Gatsby
+        &copy; {new Date().getFullYear()} BregaCode. All rights reserved. Built
+        with Gatsby
       </p>
     </Wrapper>
   )
