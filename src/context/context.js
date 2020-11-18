793259f576ac46795a9ab10d86bc5350e429da7f
bregarulai <bregarulai@gmail.com>
2020-11-18 16:21:26 -0500

    initial setup for sidebar component

--- src/context/context.js
+++ src/context/context.js
@@ -4,8 +4,10 @@ import sublinks from "../constants/links"
 const GatsbyContext = React.createContext()
 
 const GatsbyProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true)
+  const [links, setLinks] = useState(sublinks)
   return (
-    <GatsbyContext.Provider value="hello world">
+    <GatsbyContext.Provider value={{ isSidebarOpen, links }}>
       {children}
     </GatsbyContext.Provider>
   )
